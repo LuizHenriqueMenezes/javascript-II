7fef0a36c9993577315200ed37738d899237d8d2
Luiz Henrique <lmenezesmessias@gmail.com>
2023-07-19 15:28:23 -0300

    sobe

---
+++ conteudo/eventosNoHTML/eventos.js
@@ -0,0 +1,10 @@
+let n = 0
+
+function testeBotao(){
+    n += 1
+    if(n === 1){
+    alert("clicou no botão " + n + " vez")
+    }else{
+        alert("clicou no botão " + n + " vezes")
+    }
+}
